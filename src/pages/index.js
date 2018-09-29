a51e06ffa303603e9f9e1595f80a5e306523c808
Gerardo Fernandez <gerardojose13@gmail.com>
2018-09-29 10:43:56 -0700

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+import Layout from '../components/layout'
+
+const IndexPage = () => (
+  <Layout>
+    <h1>Hi people</h1>
+    <p>Welcome to my new Gatsby site!</p>
+    <p>
+      My Name is Gerardo Fernandez. I have recently graduated from the UC
+      Berkeley extention Full Stack Web Development bootcamp and I'm in the
+      process of finding my first developer job in the Bay Area.
+    </p>
+    <p>
+      I have also done many of the modules now present in FreeCodeCamp and I
+      have really enjoyed the big community FreeCodeCamp has created.
+    </p>
+    <p>I look forward to this, my first, hackathon!</p>
+  </Layout>
+)
+
+export default IndexPage
